5f06bc7f0a8554033bbd5f957da4f03e8c23c24b
khaiminh0401 <khaiminh0401@gmail.com>
2023-01-08 23:13:04 +0700

    demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
 function App() {
+  const [data,setData] = useState({});
+  const hanldeSubmit = (e)=>{
+      e.preventDefault();
+  }
+  const handleChange = (e)=>{
+    let name = e.target.name;
+    let value = e.target.value;
+    setData({...data,[name]:value})
+    console.log(data);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={hanldeSubmit}>
+          Username: <input type="text" name="username" 
+                    onChange={handleChange} /> <br />
+          Password: <input type="password" name="password" onChange={handleChange} /> <br />
+          <button>Submit</button>
+        </form>
     </div>
   );
 }
